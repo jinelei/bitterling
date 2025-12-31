fa8f3a9dc59f872c71ed4bfe6920920e21947bb8
jinelei <jinelei@163.com>
2025-12-31 15:49:16 +0800

    移除无用导入

--- web/src/main/java/com/jinelei/bitterling/web/WebApplication.java
+++ web/src/main/java/com/jinelei/bitterling/web/WebApplication.java
@@ -1,9 +1,7 @@
 package com.jinelei.bitterling.web;
 
-import com.jinelei.bitterling.core.config.GlobalExceptionHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 //@Import(GlobalExceptionHandler.class)
