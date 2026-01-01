2712075303858373c025870af13be928d9622c08
jinelei <jinelei@163.com>
2026-01-01 23:57:25 +0800

    关闭banner&优化日志

--- web/src/main/java/com/jinelei/bitterling/web/WebApplication.java
+++ web/src/main/java/com/jinelei/bitterling/web/WebApplication.java
@@ -1,32 +1,21 @@
 package com.jinelei.bitterling.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.net.URL;
-import java.util.Enumeration;
-
 @SpringBootApplication
-//@Import(GlobalExceptionHandler.class)
 public class WebApplication implements CommandLineRunner {
+    private final Logger log = LoggerFactory.getLogger(WebApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
 
     @Override
-    public void run(String... args) throws Exception {
-        // 查找classpath中所有的data.sql
-        Enumeration<URL> resources = getClass().getClassLoader().getResources("database/data.sql");
-        if (resources.hasMoreElements()) {
-            System.out.println("找到data.sql，路径：" + resources.nextElement());
-        } else {
-            System.out.println("未找到data.sql！当前classpath包含：");
-            // 打印所有classpath路径，确认resources是否在其中
-            Enumeration<URL> allResources = getClass().getClassLoader().getResources("");
-            while (allResources.hasMoreElements()) {
-                System.out.println(allResources.nextElement());
-            }
-        }
+    public void run(String... args) {
+        log.warn("系统启动成功!");
     }
 }
