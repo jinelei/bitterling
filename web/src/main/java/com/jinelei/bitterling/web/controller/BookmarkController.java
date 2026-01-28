70b3c952726823c30f80eb26cbfd5539e382b2e5
jinelei <jinelei@gmail.com>
2026-01-28 21:18:52 +0800

    添加utf8编码

--- web/src/main/java/com/jinelei/bitterling/web/controller/BookmarkController.java
+++ web/src/main/java/com/jinelei/bitterling/web/controller/BookmarkController.java
@@ -7,11 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.jinelei.bitterling.core.controller.BaseController;
@@ -26,7 +22,7 @@ import com.jinelei.bitterling.web.service.MessageService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-@Controller
+@RestController
 @RequestMapping("/bookmark")
 @Tag(name = "书签管理", description = "书签相关接口")
 public class BookmarkController extends BaseController {
