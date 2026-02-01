f7e545df86abbe1a28169f7c892757c20749cc2d
jinelei <jinelei@gmail.com>
2026-02-01 11:15:41 +0800

    添加查询我的信息

--- src/main/java/com/jinelei/bitterling/service/UserService.java
+++ src/main/java/com/jinelei/bitterling/service/UserService.java
@@ -9,10 +9,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import java.security.Principal;
