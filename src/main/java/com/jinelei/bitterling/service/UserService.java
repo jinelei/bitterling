0e53a2c3ecc30d193c5a9e0cce8327ee8930b8df
jinelei <jinelei@gmail.com>
2026-02-01 11:15:26 +0800

    添加查询我的信息

--- src/main/java/com/jinelei/bitterling/service/UserService.java
+++ src/main/java/com/jinelei/bitterling/service/UserService.java
@@ -5,7 +5,10 @@ import com.jinelei.bitterling.domain.dto.UserInfoResponse;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
@@ -13,6 +16,7 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import java.security.Principal;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Optional;
 
@@ -20,31 +24,41 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserService {
     private final Logger log = LoggerFactory.getLogger(UserService.class);
-    private final UserDetailsService userDetailsService;
 
     public UserInfoResponse getUserInfo(Principal principal) {
+        UsernamePasswordAuthenticationToken authorization = Optional.ofNullable(principal)
+                .filter(i -> i instanceof UsernamePasswordAuthenticationToken)
+                .map(UsernamePasswordAuthenticationToken.class::cast)
+                .orElseThrow(() -> new BusinessException("获取当前登录用户失败"));
+        return getUserInfo(authorization);
+    }
+
+    public UserInfoResponse getUserInfo(Authentication authentication) {
         final UserInfoResponse response = new UserInfoResponse();
-        final String username = Optional.ofNullable(principal)
-                .map(Principal::toString)
-                .filter(StringUtils::hasLength)
-                .orElseThrow(() -> new BusinessException("获取当前登录用户名失败"));
-        response.setUsername(username);
-        final UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-        Optional.ofNullable(userDetails).orElseThrow(() -> new BusinessException("获取登录用户信息失败"));
         response.setPermissions(new HashSet<>());
         response.setRoles(new HashSet<>());
-        if (!CollectionUtils.isEmpty(userDetails.getAuthorities())) {
-            userDetails.getAuthorities()
-                    .stream().map(GrantedAuthority::getAuthority)
-                    .filter(StringUtils::hasLength)
-                    .forEach(role -> {
-                        if (role.startsWith("ROLE_")) {
-                            response.getRoles().add(role);
-                        } else {
-                            response.getPermissions().add(role);
-                        }
-                    });
-        }
+        User user = Optional.ofNullable(authentication)
+                .map(Authentication::getPrincipal)
+                .filter(i -> i instanceof User)
+                .map(User.class::cast)
+                .orElseThrow(() -> new BusinessException("获取当前登录用户失败"));
+        Optional.ofNullable(user)
+                .map(User::getUsername)
+                .filter(StringUtils::hasLength)
+                .ifPresent(response::setUsername);
+        Optional.ofNullable(user)
+                .map(User::getAuthorities)
+                .stream()
+                .flatMap(Collection::stream)
+                .map(GrantedAuthority::getAuthority)
+                .filter(StringUtils::hasLength)
+                .forEach(role -> {
+                    if (role.startsWith("ROLE_")) {
+                        response.getRoles().add(role);
+                    } else {
+                        response.getPermissions().add(role);
+                    }
+                });
         return response;
     }
 
