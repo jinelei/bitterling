335ab54eb346e2aecb37cf8f4f8d89652ef94d96
jinelei <jinelei@163.com>
2026-03-03 18:05:07 +0800

    尝试添加编辑

--- src/main/java/com/jinelei/bitterling/config/SecurityConfig.java
+++ src/main/java/com/jinelei/bitterling/config/SecurityConfig.java
@@ -10,8 +10,6 @@ import com.jinelei.bitterling.domain.result.StringResult;
 import com.jinelei.bitterling.utils.MD5Util;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -74,6 +72,7 @@ public class SecurityConfig {
                         new SimpleGrantedAuthority("PAGE_/about"),
                         new SimpleGrantedAuthority("PAGE_/memo"),
                         new SimpleGrantedAuthority("PAGE_/memo_detail"),
+                        new SimpleGrantedAuthority("PAGE_/memo_edit"),
                         new SimpleGrantedAuthority("PAGE_/bookmark")
                 ))
                 .build();
